5490b9c6fcd4e5c87544e6d79da4585de836e1d0
fono09 <fono@fono.jp>
2014-12-08 11:54:31 +0900

    まず手を付けるじゃろ？

--- Tohbaq.pl
+++ Tohbaq.pl
@@ -3,25 +3,15 @@
 use strict;
 use warnings;
 
+use KiokuDB;
+#ORMをｽﾙﾉﾃﾞｽ
+
 use CGI;
 use CGI::Session;
 #セッションとかフォーム入力とかとても簡単にする
 
 use Net::Twitter;
 # 各種認証とREST_API
-
-use DBI;
-=pod
-master_idに親垢のidを設定する、master_idがNULLなら親垢
-+---------------------+----------+------+-----+---------+----------------+
-| Field               | Type     | Null | Key | Default | Extra          |
-+---------------------+----------+------+-----+---------+----------------+
-| id                  | int(11)  | NO   | PRI | NULL    | auto_increment |
-| master_id           | int(11)  | YES  |     | NULL    |                |
-| access_token        | tinytext | NO   |     | NULL    |                |
-| access_token_secret | tinytext | NO   |     | NULL    |                |
-+---------------------+----------+------+-----+---------+----------------+
-=cut
 
 use utf8;
 use Encode;
@@ -30,19 +20,37 @@ use Encode;
 use XML::Simple;
 #設定ファイル用
 
-our $dbh = DBI->connect('DBI:mysql:Tohbaq:localhost:3306','tohbaq','39tanprpr');
-#GRANT済みだし外から叩け無いので安心
+our $kdb = KiokuDB->connect('dbi:SQLite:dbname=Tohbaq.db');
+#KiokuDB使います
 
 binmode(STDIN,":utf8");
 binmode(STDOUT,":utf8");
 binmode(STDERR,":utf8");
-$dbh->do("set names utf8");
-=pod
-お決まりのutf-8対応
-=cut
+#お決まりのutf-8対応
 
-our $argument = $EVN{'QUIERY_STRING'};
+our $argument = $ENV{'QUIERY_STRING'};
 
 my $cgi = new CGI;
 
-my $consumer_key = 
+
+
+
+{
+	package User;
+	use Moose;
+
+	has id => ( is => 'ro', isa => 'Int'); 
+	has access_token => ( is => 'ro', isa => 'Str');
+	has access_token_secret => ( is => 'ro', isa =>'Str');
+
+}
+
+{
+
+	package MasterUser;
+	use Moose;
+	use base qw/User/;
+
+	has slave => ( is => 'rw', isa => 'ArrayRef[User]');
+
+}
